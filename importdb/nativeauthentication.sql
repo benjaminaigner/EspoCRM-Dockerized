9d67d8e9d5b35e2864628a43842667ad75b28118
Martin Deinhofer <deinhofe@technikum-wien.at>
2019-09-26 13:35:28 +0200

    renamed nativeauthentication.sql to generic import.sql file

--- importdb/nativeauthentication.sql
+++ importdb/nativeauthentication.sql
@@ -1,6 +1,1 @@
--- please define the corresponding user/password here...
--- CREATE USER 'espo' IDENTIFIED  WITH mysql_native_password BY 'secret2';
-ALTER USER 'espo' IDENTIFIED WITH mysql_native_password BY 'secret2';
--- ALTER USER 'root' IDENTIFIED WITH mysql_native_password BY 'secret';
-GRANT ALL PRIVILEGES ON espocrm . * TO 'espo';
-FLUSH PRIVILEGES;
+-- Put your SQL statements for db initialization here.
